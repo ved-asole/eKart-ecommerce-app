f29190fc9ec0f374902a3a9123ca38d96f91b621
vedasole <vedasole001@gmail.com>
2023-12-13 17:24:51 +0530

    added css to app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className='container-xl text-center'>
+    <div className='container-fluid bg-dark text-white text-center py-2 px-2 py-md-3 px-md-5' data-bs-theme="dark">
+      <Header />
       <h1 className='mt-3'>eKart Ecommerce Application</h1>
     </div>
   );
