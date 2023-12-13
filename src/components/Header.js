36f84f2bfe857dd3486a4170a6015dcb9e02d8fe
vedasole <vedasole001@gmail.com>
2023-12-13 17:28:47 +0530

    added Header component

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+export default function Header() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-md">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">eKart Shopping</a>
+          <button className="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <form className="d-flex flex-fill me-2 mx-md-2 mt-2 mt-md-1 mb-1" role="search">
+            <input className="form-control" type="search" placeholder="Search for products, brands and more" aria-label="Search" />
+            <button className="btn btn-outline-success ms-1" type="submit">Search</button>
+          </form>
+          <div className="collapse navbar-md-collapse d-md-flex justify-content-md-end" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
+              <a className="nav-link" href="#">Categories</a>
+              <a className="nav-link" href="#">Login</a>
+              <a className="nav-link">
+                <FontAwesomeIcon className='me-1' icon={faCartShopping} />
+                <span>Cart</span>
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  )
+}
