ae427cd9bd11ad3c2d4c886942204726b30b7394
vedasole <vedasole001@gmail.com>
2024-01-22 01:57:08 +0530

    added homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/common/Header';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className='container-fluid bg-dark text-white text-center py-2 px-2 py-md-3 px-md-5 mt-5 mt-md-0' data-bs-theme="dark">
       <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+
+  return (
+    <div className='home container'>
+      <h1>Homepage</h1>
+    </div>
+  )
+}
+
+export default Home
