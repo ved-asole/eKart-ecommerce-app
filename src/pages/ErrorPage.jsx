b354ba420e85062d097224d2bc7fd9f45e3f48b8
Ved Asole <vedasole001@gmail.com>
2024-02-08 23:46:12 +0530

    updated for checking if router error is there

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -1,27 +1,48 @@
 import React from 'react'
-import { useRouteError } from 'react-router-dom';
+import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
 import Header from '../components/common/Header';
 
 const ErrorPage = () => {
 
   const error = useRouteError();
   console.error(error);
-
-  return (
-    <div className='container-fluid bg-dark text-white text-center py-2 px-2 py-md-3 px-md-5 mt-5 mt-md-0' data-bs-theme="dark">
-      <Header />
-      <div className='container-fluid bg-dark text-white text-center
-     position-absolute top-50 start-50 translate-middle'
-        data-bs-theme="dark">
-        <h1 className='fs-1'>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p className='text-secondary'>
-          <i>{error.statusText || error.message}</i>
-        </p>
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div className='container-fluid bg-dark text-white text-center py-2 px-2 
+      py-md-3 px-md-5 mt-5 mt-md-0' data-bs-theme="dark">
+        <Header />
+        <div className='container-fluid bg-dark text-white text-center
+          position-absolute top-50 start-50 translate-middle'
+          data-bs-theme="dark">
+          <h1 className='fs-1'>Oops!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <p className='text-secondary'>
+            <i>{error.statusText || error.message}</i>
+          </p>
+          <div>
+            <a href="/" className="btn btn-light">Go Back to Home</a>
+          </div>
+        </div>
       </div>
-    </div>
 
-  );
+    );
+  } else {
+    return (
+      <div className='container-fluid bg-dark text-white text-center py-2 px-2 
+      py-md-3 px-md-5 mt-5 mt-md-0' data-bs-theme="dark">
+        <Header />
+        <div className='container-fluid bg-dark text-white text-center
+          position-absolute top-50 start-50 translate-middle'
+          data-bs-theme="dark">
+          <h1 className='fs-1'>Oops!</h1>
+          <p>Sorry, an unexpected error has occurred.</p>
+          <div>
+            <a href="/" className="btn btn-light">Go Back to Home</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default ErrorPage
