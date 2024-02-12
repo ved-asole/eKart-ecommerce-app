f51c67b95687c0b1d3dafbd3b12d22602720c507
vedasole <vedasole001@gmail.com>
2024-02-13 01:10:41 +0530

    removed unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { Outlet, ScrollRestoration, useNavigation } from 'react-router-dom';
-import './App.css';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer';
 import AppLoader from './components/common/AppLoader';
@@ -16,10 +15,11 @@ function App() {
   else {
 
     return (
-      <div className='container-fluid bg-dark text-white text-center py-2 px-2 py-md-3 px-md-5 mt-5 mt-md-0' data-bs-theme="dark">
+      <div className='container-fluid bg-dark text-white text-center py-2 px-2 py-md-3 px-md-5 mt-5 mt-md-0 h-100'
+        data-bs-theme="dark">
         <ScrollRestoration />
         <Header />
-        <Outlet />
+          <Outlet />
         <Footer />
       </div>
     );
