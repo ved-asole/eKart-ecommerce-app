4ee8fbde429435dc1d250382153c9aa29683b18d
vedasole <vedasole001@gmail.com>
2024-02-21 22:01:32 +0530

    updated products folder name and moved filters. Added getFilteredProducts method to get filtered products.

--- src/components/products/Filters.jsx
+++ src/components/products/Filters.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchProducts } from '../../redux/slices/productsSlice';
 
-const Filters = ({ setFilteredProducts }) => {
+const Filters = ({ getFilteredProducts, setFilteredProducts }) => {
 
   const categories = useSelector((state) => state.categories.categories)
   const products = useSelector((state) => state.products.products)
@@ -20,23 +20,23 @@ const Filters = ({ setFilteredProducts }) => {
 
   const updateSorting = (event) => {
     const sortingType = event.target.value;
-    const arrayForSorting = [...products];
+    const arrayForSorting = [...getFilteredProducts()];
 
     switch (sortingType) {
       case 'none':
         setFilteredProducts(products);
         break;
       case 'low-to-high-price':
-        setFilteredProducts(arrayForSorting.sort((product1, product2) => product1.price - product2.price));
+        setFilteredProducts(arrayForSorting.sort((product1, product2) => Math.trunc(product1.price * (1 - product1.discount / 100)) - Math.trunc(product2.price * (1 - product2.discount / 100))));
         break;
       case 'high-to-low-price':
-        setFilteredProducts(arrayForSorting.sort((product1, product2) => product2.price - product1.price));
+        setFilteredProducts(arrayForSorting.sort((product1, product2) => Math.trunc(product2.price * (1 - product2.discount / 100)) - Math.trunc(product1.price * (1 - product1.discount / 100))));
         break;
       case 'discount':
         setFilteredProducts(arrayForSorting.sort((product1, product2) => product2.discount - product1.discount));
         break;
       default:
-        setFilteredProducts(products);
+        setFilteredProducts(arrayForSorting);
         break;
     }
   }
@@ -52,34 +52,36 @@ const Filters = ({ setFilteredProducts }) => {
   return (
     <div>
       <p className='fw-medium'>Filters :</p>
-      <div className='mt-3'>
-        <div className='mb-3'>Categories :</div>
-        {categories.map((category) => <p key={category.categoryId} className='fs-6 ms-3 lh-1'>{category.name}</p>)}
-      </div>
-      <div className='pe-3 py-3 border-top'>
-        <label htmlFor="priceRange" className="form-label">Price :</label>
-        <input type="range" className="form-range" min={0} max={100000} step={1000} id="priceRange" value={100000} onChange={priceChange}></input>
-        <div className="d-flex flex-wrap justify-content-between">
-          <span>Min: {minPrice}</span>
-          <span>to</span>
-          <span>{maxPrice}</span>
+      <div className="ps-2">
+        <div className='mt-3'>
+          <div className='mb-3'>Categories :</div>
+          {categories.map((category) => <p key={category.categoryId} className='fs-6 ms-3 lh-1'>{category.name}</p>)}
         </div>
-      </div>
-      <div className='py-3 border-top'>
+        <div className='pe-3 py-3 border-top'>
+          <label htmlFor="priceRange" className="form-label">Price :</label>
+          <input type="range" className="form-range" min={0} max={100000} step={1000} id="priceRange" value={maxPrice} onChange={priceChange}></input>
+          <div className="d-flex flex-wrap justify-content-between">
+            <span>Min: {minPrice}</span>
+            <span>to</span>
+            <span>{maxPrice}</span>
+          </div>
+        </div>
+        <div className='py-3 border-top'>
+        </div>
         <div className='mb-3'>Sort By :</div>
         <div>
           <div className="btn-group-vertical" role="group" aria-label="Sort By Options">
             <input type="radio" className="btn-check" name="sortBy" id="sortBy-none"
-              value={'none'} autoComplete="off" defaultChecked={true} onClick={updateSorting} />
+              value='none' autoComplete="off" defaultChecked={true} onClick={updateSorting} />
             <label className="btn btn-outline-dark text-light" htmlFor="sortBy-none">None</label>
             <input type="radio" className="btn-check" name="sortBy" id="sortBy-low-to-high-price"
-              value={'low-to-high-price'} autoComplete="off" onClick={updateSorting} />
+              value='low-to-high-price' autoComplete="off" onClick={updateSorting} />
             <label className="btn btn-outline-dark text-light" htmlFor="sortBy-low-to-high-price">Low to High Price</label>
             <input type="radio" className="btn-check" name="sortBy" id="sortBy-high-to-low-price"
-              value={'high-to-low-price'} autoComplete="off" onClick={updateSorting} />
+              value='high-to-low-price' autoComplete="off" onClick={updateSorting} />
             <label className="btn btn-outline-dark text-light" htmlFor="sortBy-high-to-low-price">High to Low Price</label>
             <input type="radio" className="btn-check" name="sortBy" id="sortBy-discount"
-              value={'discount'} autoComplete="off" onClick={updateSorting} />
+              value='discount' autoComplete="off" onClick={updateSorting} />
             <label className="btn btn-outline-dark text-light" htmlFor="sortBy-discount">By Discount</label>
           </div>
         </div>
