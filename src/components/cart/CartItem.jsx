ad9e6f40b2acfbd7e0c4323a11dc9675916e6503
vedasole <vedasole001@gmail.com>
2024-04-27 03:44:00 +0530

    added mobile view responsiveness to cart item images

--- src/components/cart/CartItem.jsx
+++ src/components/cart/CartItem.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { removeFromCart, updateCart } from '../../redux/slices/cartSlice';
 import { useDispatch } from 'react-redux';
+import { getFormattedPrice } from '../../util/appUtil';
 
 const CartItem = ({ cartItem }) => {
 
   const dispatch = useDispatch();
+  const [isMobileSize, setIsMobileSize] = useState(false)
+
+  //choose the screen size 
+  const handleResize = () => {
+    if (window.innerWidth < 720) {
+      setIsMobileSize(true)
+    } else {
+      setIsMobileSize(false)
+    }
+  }
 
   const updateCountToAddToCart = (event) => {
     let tempCartItem = { ...cartItem };
@@ -21,24 +32,31 @@ const CartItem = ({ cartItem }) => {
     }
   }
 
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, [])
+
   return (
+
     <div key={cartItem.productId}
-      className='d-flex mb-4 mx-md-5 justify-content-between 
-      justify-content-md-evenly gap-4 pb-4 border-bottom'>
-      <div className='col-md-6 mx-3 mx-md-0 text-center'>
+      className='d-flex mb-4 mx-md-4 justify-content-between 
+      justify-content-md-evenly align-items-center gap-4 pb-4 border-bottom'>
+      <div className='col-4 col-md-6 mx-3 mx-md-0 text-center'>
         <img className='rounded-3' src={cartItem.image} alt={cartItem.name}
-          height={"160px"}
+          height={isMobileSize < 768 ? "140px" : "560px"}
         />
       </div>
       <div className='col-md-6 mt-1 mx-3 mx-md-0'>
-        <p>{cartItem.name}</p>
+        <p>
+          {cartItem.name}
+        </p>
         {/* <div className='d-flex justify-content-between'>
           <p>{cartItem.name}</p>
           <p>Delivery by </p>
         </div> */}
         <p>
-          <span className='fs-md-5 fw-medium'>₹{Math.trunc(cartItem.price * (1 - cartItem.discount / 100))}</span>
-          <span className='mx-2 text-decoration-line-through'>{cartItem.price}</span>
+          <span className='fs-md-5 fw-medium'>₹{getFormattedPrice(Math.trunc(cartItem.price * (1 - cartItem.discount / 100)))}</span>
+          <span className='mx-2 text-decoration-line-through'>{getFormattedPrice(cartItem.price)}</span>
           <span className='fs-md-6 fw-bold'>{`${cartItem.discount}% Off`}</span>
         </p>
         <div className="cart-count mt-3 d-inline-block">
@@ -49,7 +67,8 @@ const CartItem = ({ cartItem }) => {
           <button className='btn btn-dark px-3 py-1 rounded-end-2' name='increaseCount'
             onClick={updateCountToAddToCart}
           >+</button>
-          <button className='fw-medium ms-5 pt-4 bg-transparent border-0' onClick={(event) => dispatch(removeFromCart(cartItem))}>REMOVE</button>
+          <button className='fw-medium ms-md-5 pt-4 bg-transparent border-0'
+            onClick={(event) => dispatch(removeFromCart(cartItem))}>REMOVE</button>
         </div>
       </div>
     </div >
