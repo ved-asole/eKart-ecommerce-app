6abbb38ed02cc208e363e36da6e21b83b87a565e
vedasole <vedasole001@gmail.com>
2024-05-01 04:19:28 +0530

    updated cart to store the cart details in the localStorage and use formatted prices

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -12,6 +12,13 @@ const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
+    getPreviousCart: (state) => {
+      state.cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+      state.cartTotalQuantity = Number(localStorage.getItem('cartTotalQuantity') || 0);
+      state.cartTotalPrice = Number(localStorage.getItem('cartTotalPrice') || 0);
+      state.cartTotalDiscount = Number(localStorage.getItem('cartTotalDiscount') || 0);
+      state.cartFinalAmount = Number(localStorage.getItem('cartFinalAmount') || 0);
+    },
     addToCart: (state, action) => {
       const itemIndex = state.cartItems.findIndex((cartItem) => cartItem.productId === action.payload.productId)
       if (itemIndex >= 0) state.cartItems[itemIndex].countToAddInCart += action.payload.countToAddInCart;
@@ -23,6 +30,12 @@ const cartSlice = createSlice({
       state.cartTotalPrice += action.payload.price * action.payload.countToAddInCart;
       state.cartTotalDiscount += action.payload.price * action.payload.discount / 100 * action.payload.countToAddInCart;
       state.cartFinalAmount += action.payload.price * (1 - action.payload.discount / 100) * action.payload.countToAddInCart;
+      localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
+      localStorage.setItem('cartTotalQuantity', state.cartTotalQuantity);
+      localStorage.setItem('cartTotalPrice', state.cartTotalPrice);
+      localStorage.setItem('cartTotalDiscount', state.cartTotalDiscount);
+      localStorage.setItem('cartFinalAmount', state.cartFinalAmount);
+
     },
     updateCart: (state, action) => {
       const itemIndex = state.cartItems.findIndex((cartItem) => cartItem.productId === action.payload.productId)
@@ -32,6 +45,11 @@ const cartSlice = createSlice({
       state.cartTotalPrice += action.payload.price * cartQuantityDiff;
       state.cartTotalDiscount += action.payload.price * action.payload.discount / 100 * cartQuantityDiff;
       state.cartFinalAmount += action.payload.price * (1 - action.payload.discount / 100) * cartQuantityDiff;
+      localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
+      localStorage.setItem('cartTotalQuantity', state.cartTotalQuantity);
+      localStorage.setItem('cartTotalPrice', state.cartTotalPrice);
+      localStorage.setItem('cartTotalDiscount', state.cartTotalDiscount);
+      localStorage.setItem('cartFinalAmount', state.cartFinalAmount);
     },
     removeFromCart: (state, action) => {
       const itemIndex = state.cartItems.findIndex((cartItem) => cartItem.productId === action.payload.productId)
@@ -43,6 +61,11 @@ const cartSlice = createSlice({
         state.cartTotalDiscount -= action.payload.price * action.payload.discount / 100 * cartItem.cartQuantity;
         state.cartFinalAmount -= action.payload.price * (1 - action.payload.discount / 100) * cartItem.cartQuantity;
         state.cartItems.splice(itemIndex, 1);
+        localStorage.setItem('cartItems', JSON.stringify(state.cartItems));
+        localStorage.setItem('cartTotalQuantity', state.cartTotalQuantity);
+        localStorage.setItem('cartTotalPrice', state.cartTotalPrice);
+        localStorage.setItem('cartTotalDiscount', state.cartTotalDiscount);
+        localStorage.setItem('cartFinalAmount', state.cartFinalAmount);
       }
     },
     clearCart: (state) => {
@@ -51,10 +74,11 @@ const cartSlice = createSlice({
       state.cartTotalPrice = 0;
       state.cartTotalDiscount = 0;
       state.cartFinalAmount = 0;
+      localStorage.clear();
     }
   }
 })
 
-export const { addToCart, updateCart, removeFromCart, clearCart } = cartSlice.actions;
+export const { getPreviousCart, addToCart, updateCart, removeFromCart, clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
