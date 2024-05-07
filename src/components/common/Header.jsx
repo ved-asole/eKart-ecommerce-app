a6fbbf6a82f2f7e89ff4b75feb5076bdce53bcb8
vedasole <vedasole001@gmail.com>
2024-05-07 23:07:15 +0530

    Refactor Header component to include search functionality

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -2,14 +2,17 @@ import { faCartShopping, faMagnifyingGlass } from '@fortawesome/free-solid-svg-i
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, NavLink } from 'react-router-dom'
-import { Collapse } from 'bootstrap';
-import { useEffect } from 'react';
+import { Collapse, Dropdown } from 'bootstrap';
+import { useEffect, useState } from 'react';
 import { getPreviousCart } from '../../redux/slices/cartSlice';
+import fetchData from '../../util/DataFetcher';
 
 export default function Header() {
 
   let cartTotalQuantity = useSelector((state) => state.cart.cartTotalQuantity);
   const dispatch = useDispatch();
+  const [searchKey, setSearchKey] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     dispatch(getPreviousCart());
@@ -30,6 +33,35 @@ export default function Header() {
     }
   }
 
+  const toggleSearchBar = (event) => {
+    setSearchKey('');
+    const dropdownElementList = document.querySelectorAll('#searchBar');
+    [...dropdownElementList].map(dropdownToggleEl => Dropdown.getOrCreateInstance(dropdownToggleEl).toggle());
+  }
+
+  const handleSearchKey = (event) => {
+    event.preventDefault();
+    setSearchKey(event.target.value);
+    if (searchKey === '') {
+      setSearchResults([]);
+      const dropdownElementList = document.querySelectorAll('#searchBar');
+      [...dropdownElementList].map(dropdownToggleEl => Dropdown.getOrCreateInstance(dropdownToggleEl).hide());
+    }
+    else {
+      fetchData(
+        `products/search?searchKey=${searchKey}`,
+        (data) => setSearchResults(data._embedded.products.map((product) => {
+          return { ...product, name: product.name.substring(0, product.name.indexOf('(')) }
+        })),
+        (errorMsg) => {
+          setSearchResults([])
+        }
+      )
+      const dropdownElementList = document.querySelectorAll('#searchBar');
+      [...dropdownElementList].map(dropdownToggleEl => Dropdown.getOrCreateInstance(dropdownToggleEl).show());
+    }
+  }
+
   return (
     <header style={{ 'marginTop': "65px" }} >
       <nav className="navbar navbar-expand-md fixed-top bg-secondary-subtle">
@@ -42,12 +74,32 @@ export default function Header() {
           <button id='navbarToggler' className="navbar-toggler me-md-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
-          <form className="d-flex flex-fill me-md-2 mx-md-2 mt-2 mt-md-1 mb-1" role="search">
-            <input className="form-control" type="search" placeholder="Search for products, brands and more" name="search" aria-label="Search" />
-            <button className="btn btn-outline-success ms-1" aria-label='search' type="submit">
+          <div id='searchBar' className="d-flex flex-fill me-md-2 mx-md-2 mt-2 mt-md-1 mb-1 dropdown" role="search">
+            <input onKeyUp={handleSearchKey} onChange={e => setSearchKey(e.target.value)}
+              className="form-control" type="search" autoComplete="false"
+              placeholder="Search for products, brands and more"
+              data-bs-toggle="dropdown" data-bs-auto-close="true"
+              name="search" aria-label="Search" value={searchKey}
+            />
+            <ul className="dropdown-menu">
+              {
+                searchResults.length > 0 ?
+                  searchResults.map((result) =>
+                    <Link key={result.productId} className="dropdown-item"
+                      onClick={toggleSearchBar}
+                      to={`/products/${result.productId}`}>{result.name}</Link>
+                  )
+                  :
+                  <li className="dropdown-item" onClick={toggleSearchBar}>
+                    <p className="dropdown-item-text text-body-secondary mb-0 py-0">No results found</p>
+                  </li>
+              }
+            </ul>
+            <button className="btn btn-outline-success ms-1 "
+              aria-label='search' onClick={handleSearchKey}>
               <FontAwesomeIcon icon={faMagnifyingGlass} size="lg" />
             </button>
-          </form>
+          </div>
           <div className="collapse navbar-md-collapse d-md-flex" id="navbarNavAltMarkup">
             <div className="navbar-nav fw-medium">
               <NavLink className="nav-link mx-1" aria-current="page" to={"/"}
