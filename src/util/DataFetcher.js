216b22d092745e81c278057889cab179c8e57a69
vedasole <vedasole001@gmail.com>
2024-07-11 23:07:08 +0530

    chore: Refactor DataFetcher.js to handle API retries

--- src/util/DataFetcher.js
+++ src/util/DataFetcher.js
@@ -19,53 +19,51 @@ export const fetchData = (path, successFunction, failureFunction, arrayName) => 
 }
 
 const fetchApiData = async (path, successFunction, failureFunction) => {
-
-  try {
-    let url = import.meta.env.VITE_API_URL.concat(path);
-    // console.info("Data fetching for url : " + url);
-    const response = await axios.get(url);
-    // console.info("Data fetched successfully for " + path);
-    successFunction(response.data);
-    return true;
-  } catch (err) {
-    // console.error("Data fetched failure for " + path + "\n error : \"" + err.message + "\"");
-    failureFunction(err.message);
-    return false;
+  let url = import.meta.env.VITE_API_URL.concat(path);
+  let retryCount = 0;
+  let errorMessage = '';
+  while (retryCount <= 2) {
+    try {
+      const response = await axios.get(url);
+      successFunction(response.data);
+      return true;
+    } catch (error) {
+      errorMessage = error.message;
+      retryCount++;
+      await new Promise(resolve => setTimeout(resolve, 1000)); // wait for 1 second before retrying
+    }
   }
-
-
+  failureFunction(errorMessage);
+  return false;
 }
 
 const fetchMultipleApiData = async (path, successFunction, failureFunction, arrayName) => {
+  let url = import.meta.env.VITE_API_URL.concat(path);
 
-  try {
-    let url = import.meta.env.VITE_API_URL.concat(path);
-    // console.info("Multiple Data fetching for url : " + url);
-    const response = await axios.get(url);
-    // console.info("Data fetched successfully for " + path);
-    // console.info("Data  : " + response.data._embedded[arrayName]);
-    successFunction(response.data._embedded[arrayName]);
-    return true;
-  } catch (err) {
-    // console.error("Data fetched failure for " + path + "\n error : \"" + err.message + "\"");
-    failureFunction(err.message);
-    return false;
+  let retryCount = 0;
+  let errorMessage = '';
+  while (retryCount <= 2) {
+    try {
+      const response = await axios.get(url);
+      successFunction(response.data._embedded[arrayName]);
+      return true;
+    } catch (error) {
+      errorMessage = error.message;
+      retryCount++;
+      await new Promise(resolve => setTimeout(resolve, 1000)); // wait for 1 second before retrying
+    }
   }
-
+  failureFunction(errorMessage);
+  return false;
 }
 
 export const fetchLocalData = async (path, successFunction, failureFunction) => {
-
-
   try {
     let url = BASE_APP_JSON_URL.concat(path).concat(JSON_EXTENSION)
-    // console.log("Sending request to url : " + url);
     const response = await axios.get(url);
-    // console.info("Data fetched successfully for " + path);
     successFunction(response.data)
     return true;
   } catch (err) {
-    // console.error("Data fetched failure for " + path + "\n error : \"" + err.message + "\"");
     failureFunction(err.message)
     return false;
   }
