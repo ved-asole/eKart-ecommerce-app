dfa87d030c7e44a45904032f0112bf036e2dfbf4
vedasole <vedasole001@gmail.com>
2024-08-11 12:12:36 +0530

    chore: Update Pagination component to fix page number calculation and use button elements instead of anchor elements

--- src/components/products/Pagination.jsx
+++ src/components/products/Pagination.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Pagination = ({ currentPage, setCurrentPage, productsPerPage, totalProducts }) => {
 
   const pageNumbers = [];
-  for (let i = 1; i < Math.floor(totalProducts / productsPerPage); i++) {
+  for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
     pageNumbers.push(i);
   }
 
@@ -11,17 +11,17 @@ const Pagination = ({ currentPage, setCurrentPage, productsPerPage, totalProduct
     <nav className='my-4' aria-label="Page Navigation">
       <ul className="pagination justify-content-center">
         <li key={'previous'} className={currentPage === 1 ? 'page-item disabled' : 'page-item'} >
-          <a href='#' className="page-link">Previous</a>
+          <button onClick={() => setCurrentPage(currentPage - 1)} className="page-link">Previous</button>
         </li>
         {
           pageNumbers.map((pageNumber) =>
             <li key={pageNumber} className="page-item">
-              <a href='#' className="page-link" onClick={() => setCurrentPage(pageNumber)} >{pageNumber}</a>
+              <button className="page-link" onClick={() => setCurrentPage(pageNumber)} >{pageNumber}</button>
             </li>
           )
         }
-        <li key={'next'} className={currentPage >= Math.floor(totalProducts / productsPerPage) - 1 ? 'page-item disabled' : 'page-item'} >
-          <a href='#' className="page-link">Next</a>
+        <li key={'next'} className={currentPage >= Math.ceil(totalProducts / productsPerPage) ? 'page-item disabled' : 'page-item'} >
+          <button onClick={() => setCurrentPage(currentPage + 1)} className="page-link">Next</button>
         </li>
       </ul>
     </nav >
