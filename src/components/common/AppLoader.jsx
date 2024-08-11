20a2c3c4af0e209ed93970a53af92bc339bd0789
vedasole <vedasole001@gmail.com>
2024-08-11 12:04:14 +0530

    refactor: Optimize AppLoader component and CategoriesBar component
    - Refactor AppLoader component to use useEffect and useState hooks for managing loading state
    - Remove unused useSelector imports in AppLoader component
    - Update CategoriesBar component to conditionally render AppLoader component based on loading state from Redux store
    - Remove unused isLoading variable in CategoriesBar component

--- src/components/common/AppLoader.jsx
+++ src/components/common/AppLoader.jsx
@@ -1,14 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
 
-const AppLoader = () => {
+const AppLoader = ({ status, children }) => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  // const categoriesLoading = useSelector(state => state.categories.loading);
+  // const productsLoading = useSelector(state => state.products.loading);
+  // const cartLoading = useSelector(state => state.cart.loading);
+
+  useEffect(() => {
+    // console.log("isLoading : " + status);
+    if (status !== undefined)
+      setIsLoading(status
+        // || categoriesLoading || productsLoading || cartLoading
+      );
+  }, [status
+    // , categoriesLoading, productsLoading, cartLoading
+  ]);
+
   return (
     // <div id='loader' className='container bg-body-secondary rounded bg-opacity-75'>
-    <div id='loader' className='container-fluid bg-dark text-white text-center py-2 px-2 py-md-3 px-md-5 mt-5 mt-md-0' data-bs-theme="dark">
-      <div className="d-flex justify-content-center align-items-center vh-100">
+    <div id='loader' className={
+      'container-fluid bg-dark text-white text-center' +
+        isLoading ? 'py-2 px-2 py-md-3 px-md-5 mt-5 mt-md-0' : ''
+    }
+      data-bs-theme="dark"
+    >
+      <div className={!isLoading ? "d-none" : "d-flex justify-content-center align-items-center"}
+        style={{ height: "calc(100vh - 129px)" }}>
         <div className="spinner-border">
           <span className="visually-hidden">Loading...</span>
         </div>
         <span className='ms-2 fs-5 fw-medium'>Loading...</span>
+      </div>
+      <div className={isLoading ? "d-none" : ''}>
+        {children}
       </div>
     </div>
   )
--- src/components/home/CategoriesBar.jsx
+++ src/components/home/CategoriesBar.jsx
@@ -2,11 +2,13 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchCategories } from '../../redux/slices/categoriesSlice';
+import AppLoader from '../common/AppLoader';
 
 const CategoriesBar = () => {
 
   // Getting categories from store
   const categories = useSelector(state => state.categories.categories);
+  const isLoading = useSelector(state => state.categories.loading);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,26 +16,28 @@ const CategoriesBar = () => {
       fetchCategories(dispatch)
   }, [])
 
-  return (
-    <section id="categoriesBarSection">
-      <div className='container bg-secondary-subtle rounded'>
-        <div className='row pt-3 mb-3 gap-2'>
-          {
-            categories?.map((category) =>
-              <Link to={"/products?category=" + category.categoryId} key={category.categoryId}
-                className="col-3 col-sm-2 col-lg-1 flex-fill text-decoration-none text-body-secondary link-light">
-                <img alt={category.name.concat(" Category")}
-                  className="rounded-circle m-auto mt-2-circle" width="70" height="70"
-                  src={category.image}
-                  aria-label={category.name.concat(" Category")} />
-                <p className='mt-1 fw-bold'>{category.name}</p>
-              </Link>
-            )}
+  return isLoading
+    ? <AppLoader />
+    : (
+      <section id="categoriesBarSection">
+        <div className='container bg-secondary-subtle rounded'>
+          <div className='row pt-3 mb-3 gap-2'>
+            {
+              categories?.map((category) =>
+                <Link to={"/products?category=" + category.categoryId} key={category.categoryId}
+                  className="col-3 col-sm-2 col-lg-1 flex-fill text-decoration-none text-body-secondary link-light">
+                  <img alt={category.name.concat(" Category")}
+                    className="rounded-circle m-auto mt-2-circle" width="70" height="70"
+                    src={category.image}
+                    aria-label={category.name.concat(" Category")} />
+                  <p className='mt-1 fw-bold'>{category.name}</p>
+                </Link>
+              )}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
 
-  )
+    )
 }
 
 export default CategoriesBar
