eeea364d99ddfcd90365acade3c57e8ca221c5a6
vedasole <vedasole001@gmail.com>
2024-08-17 20:54:39 +0530

    Update ProductCard component to improve price display and styling along with updating images with object-fit-contain

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -8,7 +8,7 @@ const ProductCard = ({ product: { productId, name, image, price, discount } }) =
         <div key={productId} className="card p-1 p-sm-2 mb-3" >
           <img src={image}
             className="card-img-top rounded-3 mx-auto mt-2 object-fit-contain" height={"150px"} alt={name} />
-          <div className="card-body mt-2 text-center p-2 pb-0 mx-2 ms-3" >
+          <div className="card-body mt-2 text-center p-2 pb-0 mx-2" >
             <p className="card-title fw-medium text-wrap">
               {name.length > 42 ? name.substr(0, 42) + "..." : name}
             </p>
