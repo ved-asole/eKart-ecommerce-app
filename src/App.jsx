9e740fd3a774c5a7aa2149be0821aea6cb205fb8
vedasole <vedasole001@gmail.com>
2024-08-26 20:03:57 +0530

    feat(Toast): Update App component to lazy load Footer and Toast components
    - Use React's lazy and Suspense to lazy load the Footer and Toast components in the App component
    - Improve page load performance by only loading these components when needed
    - Add Toast component to show toast notifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,33 @@
 import { Outlet, ScrollRestoration, useNavigation } from 'react-router-dom';
 import Header from './components/common/Header';
-import Footer from './components/common/Footer';
 import AppLoader from './components/common/AppLoader';
-import { Suspense } from 'react';
+import React, { lazy, Suspense } from 'react';
+const Footer = lazy(() => import('./components/common/Footer'));
+const Toast = lazy(() => import('./components/common/Toast'));
 
 function App() {
 
   //Get the page navigation
   const navigation = useNavigation();
 
-  // If page is in loading state, display loader.
-  if (navigation.state === "loading") return <AppLoader />;
-
-  // If page is not in loading state, display page. 
-  else {
-
-    return (
-      <div className='container-fluid bg-dark text-white text-center py-2 px-2 py-md-3 px-md-5 mt-5 mt-md-0 h-100'
-        data-bs-theme="dark">
-        <ScrollRestoration />
-        <Header />
+  return (
+    <div className='container-fluid bg-dark text-white text-center py-2 px-2 py-md-3 px-md-5 mt-5 mt-md-0 h-100'
+      data-bs-theme="dark">
+      <ScrollRestoration />
+      <Header />
+      <AppLoader status={navigation.state === "loading"}>
         <Suspense fallback={<AppLoader />}>
-          <div className='pt-2 pt-md-1'>
+          <section id="mainSection" className='w-100 h-100 overflow-hidden'
+          //  className='pt-2 pt-md-1'
+          >
             <Outlet />
-          </div>
+          </section>
+          <Footer />
+          <Toast />
         </Suspense>
-        <Footer />
-      </div>
-    );
-  }
+      </AppLoader>
+    </div>
+  );
 }
 
 export default App;
