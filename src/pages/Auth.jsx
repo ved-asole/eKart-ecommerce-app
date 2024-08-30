d443b0fe46bbb86f579f08896843d670d13bfc2f
vedasole <vedasole001@gmail.com>
2024-08-31 00:58:37 +0530

    refactor: Update AuthForm component to use separate auth.js functions for sign up, sign in and logout
    - Move the sign up, sign in and logout logic functions to separate auth.js file for better code organization and readability
    - Added removeUserData method to remove user tokens and cookies from browser

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,11 +1,9 @@
-import axios from 'axios';
 import React, { useEffect } from 'react'
 import { useCookies } from 'react-cookie';
 import { useForm } from 'react-hook-form'
 import { Link, useNavigate, useSearchParams } from 'react-router-dom';
-import { showToast } from '../util/appUtil';
-import { clearCart, fetchPreviousCart } from '../redux/slices/cartSlice';
 import { useDispatch } from 'react-redux';
+import { processLogout, processSignIn, processSignUp, removeUserData } from '../util/auth';
 
 const AuthForm = () => {
 
@@ -18,123 +16,18 @@ const AuthForm = () => {
   const dispatch = useDispatch();
 
   const formSubmitHandler = (data) => {
-    axios.defaults.headers.common.Authorization = '';
-    localStorage.removeItem('token');
-    removeCookie('username');
-    removeCookie('customerId');
+    removeUserData(removeCookie);
 
-    if (mode == 'signup') processSignUp(data);
-    else processSignIn(data);
+    if (mode == 'signup') processSignUp(data, setCookie, navigate);
+    else processSignIn(data, setCookie, navigate, reset);
   };
 
-  const processSignUp = (data) => {
-    axios.post(
-      import.meta.env.VITE_API_URL.concat("customers"),
-      {
-        email: data.email,
-        password: data.password,
-        firstName: data.firstName,
-        lastName: data.lastName,
-        phoneNumber: data.phoneNumber,
-        role: "USER"
-      },
-      {
-        headers: { 'Authorization': '' }
-      }
-    )
-      .then((response) => {
-        const token = response.headers.getAuthorization().replace('Bearer ', '');
-        setCookie('token', token, { httpOnly: true });
-        setCookie('customerId', response.data.customerId);
-        setCookie('username', response.data.firstName + " " + response.data.lastName);
-        setCookie('role', response.data.role);
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-        localStorage.setItem('token', token);
-        localStorage.setItem('cartId', cartId);
-        console.log("Logged in successfully");
-        showToast("Logged in successfully");
-        navigate('/');
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  const processSignIn = (data) => {
-    axios.post(
-      import.meta.env.VITE_API_URL.concat("auth/authenticate"),
-      {
-        email: data.email,
-        password: data.password
-      },
-      {
-        headers: {
-          Authorization: ''
-        }
-      }
-    )
-      .then((response) => {
-        const token = response.headers.getAuthorization().replace('Bearer ', '');
-        setCookie('customerId', response.data.customerId);
-        setCookie('username', response.data.firstName + " " + response.data.lastName);
-        setCookie('role', response.data.role);
-        axios.defaults.headers.common.Authorization = `Bearer ${token}`;
-        localStorage.setItem('token', token);
-        localStorage.setItem('cartId', response.data.cartId);
-        showToast("Logged In");
-        fetchPreviousCart(dispatch);
-        navigate('/');
-      })
-      .catch((error) => {
-        console.log(error?.response?.data?.message);
-        showToast("Invalid Credentials");
-        reset({
-          email: '',
-          password: ''
-        })
-      }
-      );
-  }
-
-  const processLogout = () => {
-    localStorage.clear();
-    removeCookie('username');
-    removeCookie('customerId');
-    removeCookie('role');
-    dispatch(clearCart());
-    showToast("Logged out successfully");
-    navigate('/');
-  }
-
-  const checkAuthentication = () => {
-    //Making api call to check if token is valid
-    axios.get(import.meta.env.VITE_API_URL.concat("auth/check-token"))
-      .then((response) => {
-        console.log(response.data);
-        showToast("Welcome back");
-        navigate('/');
-      })
-      .catch((error) => {
-        console.log(error);
-        removeCookie('username');
-        removeCookie('customerId');
-        localStorage.removeItem('token');
-      })
-  }
-
   useEffect(() => {
-    if (mode == 'logout') processLogout();
-    if (cookies.customerId != undefined) {
-      if (localStorage.getItem('token')) {
-        checkAuthentication();
-      }
-    }
-    if (isSubmitting) {
-      console.log("Form Submitting");
-    }
-    if (isSubmitSuccessful) {
-      console.log("Form Submitted Successfully");
-    }
+    if (mode == 'logout') processLogout(removeCookie, navigate, dispatch);
+    if (cookies.customerId != undefined && localStorage.getItem('token'))
+      checkAuthentication(removeCookie, navigate);
+    isSubmitting && console.log("Form Submitting");
+    isSubmitSuccessful && console.log("Form Submitted Successfully");
 
   }, [isSubmitting, isSubmitSuccessful])
 
