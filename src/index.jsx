5bfd2de9c4d4a1c8f993c36cceabb052b8b8f138
vedasole <vedasole001@gmail.com>
2024-09-06 17:18:27 +0530

    refactor: Update import order in index.jsx
    The App component import has been moved to a more logical position. Additionally, the order of the bootstrap CSS and JS imports has been adjusted.

--- src/index.jsx
+++ src/index.jsx
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import { RouterProvider } from 'react-router-dom';
+import App from './App';
 import router from './routes.jsx';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import AppLoader from './components/common/AppLoader.jsx';
 import { CookiesProvider } from 'react-cookie';
-import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
