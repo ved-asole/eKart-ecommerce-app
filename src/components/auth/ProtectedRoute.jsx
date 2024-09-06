74d11142c0f75cda523e6e28fa9a473e49281513
vedasole <vedasole001@gmail.com>
2024-09-06 17:40:38 +0530

    feat: Add ProtectedRoute component for authenticated routes
    A new component, ProtectedRoute, has been added to handle authenticated routes. This component checks if the user is authenticated and has the required role to access the route. If not, it displays an access denied message. This component improves the security and user experience of the application.

---
+++ src/components/auth/ProtectedRoute.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import AppLoader from '../common/AppLoader';
+import { useCookies } from 'react-cookie';
+import fetchData from '../../util/DataFetcher';
+import { showToast } from '../../util/appUtil';
+import { PropTypes } from 'prop-types';
+import RouteLoadError from '../../pages/RouteLoadError';
+
+const ProtectedRoute = ({ children, roles }) => {
+
+  const [cookies, setCookie, removeCookie] = useCookies();
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAccessDenied, setIsAccessDenied] = useState(false);
+
+  useEffect(() => {
+    if (!roles) roles = ['USER'];
+    if (localStorage.getItem('token')) {
+      fetchData(
+        "auth/check-token",
+        (data) => {
+          if (roles.includes(cookies['role'])) setIsAuthenticated(true)
+          else {
+            setIsAccessDenied(true);
+            showToast("Access Denied");
+          }
+        },
+        (errorMsg) => {
+          showToast("Please login first");
+          navigate('/auth');
+        }
+      )
+    } else {
+      showToast("Please login first");
+      navigate('/auth');
+    }
+  }, [isAuthenticated, isAccessDenied]);
+
+  // logic to check if the user is authenticated
+  let content = <AppLoader />;
+  if (isAccessDenied) {
+    content = <RouteLoadError message="Access Denied!" />;
+  } else {
+    content = children;
+  }
+  return content;
+}
+
+ProtectedRoute.propTypes = {
+  children: PropTypes.node.isRequired,
+  roles: PropTypes.array
+};
+
+export default ProtectedRoute;
