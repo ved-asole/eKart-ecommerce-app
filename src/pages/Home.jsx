499f0189afa7b9736832229db077737af70e0a7c
vedasole <vedasole001@gmail.com>
2024-11-21 20:46:27 +0530

    fix(Home): update container class to use min-vh-100 for better viewport handling

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React, { Suspense, lazy } from 'react';
-import AppLoader from '../components/common/AppLoader';
 
 const CategoriesBar = lazy(() => import('../components/home/CategoriesBar.jsx'));
 const HomeCarousel = lazy(() => import('../components/home/HomeCarousel.jsx'));
 const TopDeals = lazy(() => import('../components/home/TopDeals.jsx'));
+const AppLoader = lazy(() => import('../components/common/AppLoader.jsx'));
 
 const Home = () => {
 
   return (
-    <div className='home container vh-100 px-0'>
+    <div className='home container min-vh-100 px-0'>
 
       {/* CategoriesBar Section */}
       <Suspense fallback={<AppLoader />} >
