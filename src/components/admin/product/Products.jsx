89ecb7422797a156eff669bc16d06b06210b3a2a
vedasole <vedasole001@gmail.com>
2024-12-15 06:09:44 +0530

    feat(admin/products): fetch categories on component mount and update product table layout for better readability

--- src/components/admin/product/Products.jsx
+++ src/components/admin/product/Products.jsx
@@ -1,27 +1,34 @@
 import React, { lazy, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchProductsPage } from '../../../redux/slices/productsSlice.js';
+import { fetchCategories } from '../../../redux/slices/categoriesSlice.js';
 const AddProductModel = lazy(() => import('./AddProductModel.jsx'));
 const UpdateProductModel = lazy(() => import('./UpdateProductModel.jsx'));
 const Pagination = lazy(() => import('../../products/Pagination.jsx'));
+import { getFormattedPrice } from '../../../util/appUtil.js';
 
 /*
  * Products component.
- * 
+ *
  * This component renders the Admin Products section of the app.
  * It displays a table of products with their details and allows for adding, editing, and deleting products.
- * 
+ *
  * @return {JSX.Element} The rendered Products component.
  */
 const Products = () => {
 
   const products = useSelector((state) => state.products.products);
+  const categories = useSelector((state) => state.categories.categories);
   const page = useSelector((state) => state.products.page);
   const size = useSelector((state) => state.products.size);
   const totalPages = useSelector((state) => state.products.totalPages);
   const dispatch = useDispatch();
   const [productId, setProductId] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    fetchCategories(dispatch);
+  }, []);
 
   useEffect(() => {
     if (currentPage >= 0 || currentPage != page) {
@@ -33,32 +40,32 @@ const Products = () => {
 
   return (
     <div>
-      <section id="products-section">
+      <section id="products-section" className="mt-2">
         <h3>Products</h3>
         <div className="d-flex justify-content-between mb-3">
           <button className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#addProductModal">Add Product</button>
         </div>
         <div className="table-responsive">
           <table className="table table-responsive table-striped">
-            <thead className="align-middle">
+            <thead className="align-middle text-start">
               <tr>
                 <th>ID</th>
-                <th>Name</th>
+                <th className='text-center'>Name</th>
                 <th>Price</th>
-                <th className='text-wrap'>Quanity in Stock</th>
-                <th>Category</th>
-                <th>Actions</th>
+                <th className='text-nowrap'>Stock Qty</th>
+                <th className='text-center'>Category</th>
+                <th className='text-center'>Actions</th>
               </tr>
             </thead>
-            <tbody className='table-group-divider align-middle'>
+            <tbody className='table-group-divider align-middle text-start'>
               {products.map((product) => (
                 <tr key={product.productId}>
                   <td className='text-wrap'>{product.productId}</td>
-                  <td className='text-start'>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.qtyInStock}</td>
-                  <td>{product.categoryId}</td>
-                  <td className="w-25">
+                  <td className='text-start text-break'>{product.name}</td>
+                  <td>{getFormattedPrice(product.price)}</td>
+                  <td className='text-center'>{product.qtyInStock}</td>
+                  <td className='text-nowrap'>{categories.find(c => product.categoryId == c.categoryId)?.name}</td>
+                  <td className="w-25 text-center">
                     <button className="btn btn-outline-secondary me-0 me-md-2 mb-2 mb-lg-0"
                       data-bs-toggle="modal" data-bs-target="#updateProductModal"
                       onClick={() => setProductId(product.productId)}
