000dd9d272360afa4a7c3aa0ebc91d4cf7f6d3d3
vedasole <vedasole001@gmail.com>
2024-12-18 14:13:37 +0530

    fix(order): add lazy loading for improved performance in Orders component

--- src/components/admin/order/Orders.jsx
+++ src/components/admin/order/Orders.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, lazy } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { getFormattedPrice } from '../../../util/appUtil.js'
