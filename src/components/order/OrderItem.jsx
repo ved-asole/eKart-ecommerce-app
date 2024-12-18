7af686c205fbf0f8c75120dba51306ba631be96a
vedasole <vedasole001@gmail.com>
2024-12-18 23:48:43 +0530

    fix(order): add PropTypes validation and show toast on cancel action in OrderItem component

--- src/components/order/OrderItem.jsx
+++ src/components/order/OrderItem.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types';
+import { showToast } from '../../util/appUtil';
 
 const OrderItem = ({ orderItem, isLast }) => {
 
   const [isMobileSize, setIsMobileSize] = useState(false)
   const product = orderItem.product;
 
-  //choose the screen size 
+  //choose the screen size
   const handleResize = () => {
     if (window.innerWidth < 400) setIsMobileSize(true);
     else setIsMobileSize(false);
@@ -30,17 +32,28 @@ const OrderItem = ({ orderItem, isLast }) => {
         <p className='text-wrap mb-2'>
           {product.name}
         </p>
-        {/* <p className='text-body-secondary mb-0 text-sm'>Delivery by {new Date(Date.now() + 5 * 24 * 60 * 60 * 1000).toLocaleDateString()} </p> */}
         <div className="d-flex flex-wrap align-items-center">
           <p className='text-body-secondary mb-0 text-sm'>Delivery by {new Date(Date.now() + 5 * 24 * 60 * 60 * 1000).toLocaleDateString()} </p>
           <p className='ms-sm-5 pt-3'>Quantity : {orderItem.quantity}</p>
           <button className='fw-medium ms-md-5 bg-transparent border-0'
-          // onClick={}
+            onClick={() => showToast('Please cancel whole order')}
           >CANCEL</button>
         </div>
       </div>
     </div >
   )
 }
+
+OrderItem.propTypes = {
+  orderItem: PropTypes.shape({
+    product: PropTypes.shape({
+      productId: PropTypes.number.isRequired,
+      image: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    }).isRequired,
+    quantity: PropTypes.number.isRequired,
+  }).isRequired,
+  isLast: PropTypes.bool.isRequired
+};
 
 export default OrderItem
