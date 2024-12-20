41c8fb8e421fad000339bca4dd55ad545fba3e83
vedasole <vedasole001@gmail.com>
2024-12-20 11:41:54 +0530

    fix(products): add text-nowrap class to discount text in ProductCard component

--- src/components/products/ProductCard.jsx
+++ src/components/products/ProductCard.jsx
@@ -17,7 +17,7 @@ const ProductCard = ({ product: { productId, name, image, price, discount } }) =
               <div className='d-flex justify-content-evenly align-items-center mb-2 me-2'>
                 <p className='mb-0 fw-medium me-2'>₹{Math.trunc(price * (1 - discount / 100))}</p>
                 <p className='text-decoration-line-through fw-normal mb-0 me-2' style={{ "fontSize": "15px" }}>₹{price}</p>
-                <p className='mb-0' style={{ "fontSize": "14px" }}>{discount}% off</p>
+                <p className='mb-0 text-nowrap' style={{ "fontSize": "14px" }}>{discount}% off</p>
               </div>
             </div>
           </div>
